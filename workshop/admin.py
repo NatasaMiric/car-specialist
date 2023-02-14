4b1a841f53bc4e733222d887d52b9329b4c90568
NatasaMiric <natasa.miric94@gmail.com>
2023-02-14 18:35:35 +0000

    feat:Create admin panel

--- workshop/admin.py
+++ workshop/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.db import models
 from .models import Booking
 
-admin.site.register(Booking)
 
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'service_type', 'created_on')
+    search_fields = ['customer']
+    list_filter = ('created_on',)
+    actions = ['approve_bookings']
 
+    def approve_bookings(self, request, queryset):
+        queryset.update(approved=True)
