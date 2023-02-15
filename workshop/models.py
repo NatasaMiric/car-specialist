770bf6705a8957c6c749d2b6fe72812dfd6ad1a0
NatasaMiric <natasa.miric94@gmail.com>
2023-02-15 09:36:05 +0000

    refactor: Add updated date field on booking model

--- workshop/models.py
+++ workshop/models.py
@@ -6,6 +6,7 @@ SERVICE_TYPE = (
     ('Vehicle Diagnostics', 'Vehicle Diagnostics'),
     ('Start and charging system repairs', 'Start and charging system repairs'),
     ('Replacement of light bulbs', 'Replacement of light bulbs'),
+    ('AC service', 'AC service'),
     ('Clutch replacement', 'Clutch replacement'),
     ('Cambelt repair', 'Cambelt repair'),
     ('Basic service', 'Basic service'),
@@ -28,17 +29,18 @@ class Booking(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE,
                                  related_name="user_bookings")
     phone_number = models.CharField(max_length=12)
-    service_type = models.CharField(max_length=50, choices=SERVICE_TYPE, 
+    service_type = models.CharField(max_length=50, choices=SERVICE_TYPE,
                                     default='')
     day = models.DateField(default=datetime.now)
-    time = models.CharField(max_length=15, choices=AVAILABLE_TIME, 
+    time = models.CharField(max_length=15, choices=AVAILABLE_TIME,
                             default='8:00am-9:00am')
     comment = models.TextField(max_length=250)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
-        return f"{self.customer} | service: {self.service_type} | day: {self.day} | time: {self.time}"
+        return f"{self.customer} | day: {self.day} | time: {self.time}"
