a77dfcbe458c90ca47ee9f26d9cd8da884b23a9e
NatasaMiric <natasa.miric94@gmail.com>
2023-03-09 20:21:14 +0000

    feat: Create a method in views.py to prevent editing the booking to already occcupied time

--- workshop/views.py
+++ workshop/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 
 
 class HomePage(View):
@@ -20,7 +21,7 @@ class HomePage(View):
 
 class BookingView(LoginRequiredMixin, CreateView):
     """
-    View to render the booking page and data.
+    View to render the page where user creates a booking.
     """
     form_class = BookingForm
     template_name = 'booking.html'
@@ -33,15 +34,18 @@ class BookingView(LoginRequiredMixin, CreateView):
         form = self.form_class(request.POST)
         if form.is_valid():
             if (check_duplicate_booking(request.user, request.POST['day'],
-                request.POST['time'],)):
-                form.add_error(None, 'Booking is not available for this time.')
+                                        request.POST['time'],)):
+                form.add_error(
+                                None,
+                                'Selected time is not available.'
+                                'Please choose another time.')
                 return render(request, self.template_name, {'form': form})
             new_form = form.save(commit=False)
             new_form.user = request.user
             new_form.save()
             return redirect('mybookings')
         else:
-            form.add_error(None, 'The error occurred,please try again.')
+            print(form.errors)
         return render(request, self.template_name, {'form': form})
 
 
@@ -85,8 +89,26 @@ class UpdateBooking(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     form_class = BookingForm
     template_name = 'edit_booking.html'
     success_url = reverse_lazy('mybookings')
-    success_message = "Your booking was updated successfully"
+    success_message = "Your booking was updated successfully"  
 
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, instance=self.get_object())
+        if form.is_valid():
+            if (check_duplicate_booking(request.user, request.POST['day'],
+                                        request.POST['time'],
+                                        pk=self.request.GET.get('pk')
+                                        )):
+                form.add_error(
+                                None,
+                                'Selected time is not available. '
+                                'Please choose another time.')
+                return render(request, self.template_name, {'form': form})
+            form.save()            
+            return redirect('mybookings')
+        else:
+            print(form.errors)
+        return render(request, self.template_name, {'form': form})         
+       
 
 class DeleteBooking(LoginRequiredMixin, DeleteView):
     """
@@ -94,7 +116,7 @@ class DeleteBooking(LoginRequiredMixin, DeleteView):
     """
     model = Booking
     template_name = 'delete_booking.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('mybookings')
     success_message = "Your booking was deleted successfully."
 
     def delete(self, request, *args, **kwargs):
